df674ec119bc684e2878ff3009926df62deed3da
yangjian <cross874@outlook.com>
2019-09-28 13:15:29 +0800

    全流程测试

--- cleanRunStatus.js
+++ cleanRunStatus.js
@@ -1,3 +1,5 @@
-var today = new Date().getFullYear() + new Date().getMonth() + new Date().getDate()
-var statusStorage = storages.create("status")
-statusStorage.put(today,"")
+if(confirm("清除运行标记")){
+    var today = new Date().getFullYear() + new Date().getMonth() + new Date().getDate()
+    var statusStorage = storages.create("status")
+    statusStorage.put(today,"")
+}
