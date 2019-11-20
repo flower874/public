eef32baead0c913d073c53c245128a4f4f354385
cross874 <cross874@gmail.com>
2019-11-20 10:32:50 +0800

    '修改根路径'

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 function updateFiles() {
-    let root = '/storage/emulated/0/脚本/'
+    let root = '/storage/emulated/0/com.sac/'
     let path = 'public-master/'
     let gitUrl = 'https://codeload.github.com/flower874/public/zip/master'
     let r = http.get(gitUrl)
