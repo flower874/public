80632fc835f59dd649cf7e711aac0e3e5dc8b7d5
cross874 <cross874@gmail.com>
2019-12-11 21:55:11 +0800

    '增加提示'

--- update.js
+++ update.js
@@ -1,7 +1,8 @@
 function updateFiles() {
-    let root = '/storage/emulated/0/com.sac/'
+    let root = '/storage/emulated/0/脚本/'
     let path = 'public-master/'
     let gitUrl = 'https://codeload.github.com/flower874/public/zip/master'
+    toastLog("下载数据..")
     let r = http.get(gitUrl)
     let zipContent = r.body.bytes()
     let file = 'master.zip'
@@ -10,9 +11,17 @@ function updateFiles() {
     files.createWithDirs(unzip)
     files.writeBytes(unzip,zipContent)
     //pro专用
+    toastLog("解压")
     $zip.unzip(unzip,root);
     //com.stardust.io.Zip.unzip(new java.io.File(unzip), new java.io.File(root))
+    toastLog("覆盖本地文件")
     shell("cp -r "+root+path+"* "+root+".")
     return true;
 };
-updateFiles();
+
+toastLog("同步数据到本地");
+if(!updateFiles()){
+    log("本地文件升级失败")
+    return;
+};
+toastLog("同步完成")
