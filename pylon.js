7fa891ce499c2e5e955425d1c02caf13234c43c4
cross874 <cross874@gmail.com>
2019-12-14 16:05:27 +0800

    '

--- pylon.js
+++ pylon.js
@@ -1,5 +1,4 @@
 "auto";
-"ui";
 //版本
 let ver = '1.2.1';
 let root,path,gitUrl,r,zipContent,file,unzip,pylonCode
@@ -8,30 +7,26 @@ let storage = storages.create("alreadyTime");
 
 log(storage.get(today));
 
-let up = confirm("需要更新吗?");
-
-if(up){
-    toastLog("本机id : "+device.getAndroidId().slice(-6));
-    toastLog("启动器版本 : "+ver);
-    root = '/storage/emulated/0/com.sac/'
-    path = 'public-master/'
-    gitUrl = 'https://codeload.github.com/flower874/public/zip/master'
-    toastLog("下载数据..")
-    r = http.get(gitUrl)
-    zipContent = r.body.bytes()
-    file = 'master.zip'
-    unzip = files.join(root,file)
-    if(files.isDir(unzip))files.removeDir(unzip);
-    files.createWithDirs(unzip)
-    files.writeBytes(unzip,zipContent)
-    //pro专用
-    toastLog("解压")
-    $zip.unzip(unzip,root);
-    //com.stardust.io.Zip.unzip(new java.io.File(unzip), new java.io.File(root))
-    toastLog("覆盖本地文件")
-    shell("cp -r "+root+path+"* "+root+".")
-    toastLog("同步完成");
-};
+toastLog("本机id : "+device.getAndroidId().slice(-6));
+toastLog("启动器版本 : "+ver);
+root = '/storage/emulated/0/com.sac/'
+path = 'public-master/'
+gitUrl = 'https://codeload.github.com/flower874/public/zip/master'
+toastLog("下载数据..")
+r = http.get(gitUrl)
+zipContent = r.body.bytes()
+file = 'master.zip'
+unzip = files.join(root,file)
+if(files.isDir(unzip))files.removeDir(unzip);
+files.createWithDirs(unzip)
+files.writeBytes(unzip,zipContent)
+//pro专用
+toastLog("解压")
+$zip.unzip(unzip,root);
+//com.stardust.io.Zip.unzip(new java.io.File(unzip), new java.io.File(root))
+toastLog("覆盖本地文件")
+shell("cp -r "+root+path+"* "+root+".")
+toastLog("同步完成");
 
 threads.start(function(){
     let sac = {util:require('/storage/emulated/0/com.sac/util.js')};
