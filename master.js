bc7914a6a11fa341b8e82c6711d0dc0382075dc2
cross874 <cross874@gmail.com>
2019-12-15 18:42:42 +0800

    '

--- master.js
+++ master.js
@@ -9,7 +9,42 @@ customEvent.on('log',function(r){
     toastLog("收到日志 : " + r)
 });
 
-threads.start(function(){
+var memdog = threads.start(function(){
+    let msac = {util:require('/storage/emulated/0/com.sac/util.js')};
+    while(true){
+        sleep(5000);
+        let mem = device.getAvailMem()/1024/1024
+        runtime.gc()
+        if(mem<300){
+            toastLog("内存不足，强制FullGC");
+            try{t_main.interrupt();}catch(e){};
+            try{t_cancel.interrupt();}catch(e){};
+            recents();
+            sleep(100);
+            if(device.brand === 'samsung'){
+                msac.util.forcePress(id("recents_close_all_button").findOne(2000))
+            };
+            if(device.brand === 'HONOR'){
+                id("clear_all_recents_image_button").findOne(2000).click(); 
+            };
+            if(device.brand === 'OPPO'){
+                //forcePress(id("clear_panel").findOne(2000));
+                msac.util.forcePress(id("clear_button").findOne(2000));
+            };
+            if(device.brand === 'Realme'){
+                msac.util.forcePress(id("clear_all_button").findOne(2000))
+            };
+            if(device.brand === 'ZTE'){
+                sleep(1800);
+                msac.util.forcePress({x:50,y:76});
+            }; 
+        };
+        sleep(1000)
+    }
+});
+
+
+var offkey = threads.start(function(){
     events.observeKey();
     events.setKeyInterceptionEnabled("volume_down", true);
     events.on("key",(code)=>{
@@ -19,6 +54,7 @@ threads.start(function(){
         }
     });
 });
+
 //快捷键
 let ac = {util:require('/storage/emulated/0/com.sac/util.js')};
 let AppName,scriptFile,code,time
--- pylon.js
+++ pylon.js
@@ -33,40 +33,6 @@ if(up){
     toastLog("同步完成");
 };
 
-threads.start(function(){
-    let msac = {util:require('/storage/emulated/0/com.sac/util.js')};
-    while(true){
-        sleep(5000);
-        let mem = device.getAvailMem()/1024/1024
-        runtime.gc()
-        if(mem<300){
-            toastLog("内存不足，强制FullGC");
-            try{t_main.interrupt();}catch(e){};
-            try{t_cancel.interrupt();}catch(e){};
-            recents();
-            sleep(100);
-            if(device.brand === 'samsung'){
-                msac.util.forcePress(id("recents_close_all_button").findOne(2000))
-            };
-            if(device.brand === 'HONOR'){
-                id("clear_all_recents_image_button").findOne(2000).click(); 
-            };
-            if(device.brand === 'OPPO'){
-                //forcePress(id("clear_panel").findOne(2000));
-                msac.util.forcePress(id("clear_button").findOne(2000));
-            };
-            if(device.brand === 'Realme'){
-                msac.util.forcePress(id("clear_all_button").findOne(2000))
-            };
-            if(device.brand === 'ZTE'){
-                sleep(1800);
-                msac.util.forcePress({x:50,y:76});
-            }; 
-        };
-        sleep(1000)
-    }
-});
-
 pylonCode = files.read('/storage/emulated/0/com.sac/master.js');
 
 while(true){
@@ -75,6 +41,8 @@ while(true){
     }catch(e){
         console.log(e)
     };
+    offkey.interrupt();
+    memdog.interrupt();
     sleep(5000)
 };
 
