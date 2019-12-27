f0459d0cdb0adfe05c5b751d66b29a8f6aa44da1
cross874 <cross874@gmail.com>
2019-12-27 13:08:23 +0800

    '

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 //var sac={util:require('./util.js')};
+//app.viewFile("/storage/emulated/0/tencent/MicroMsg/Download/pylon_v1.2.29.apk")
+
+//安装方法
+// 打开本机商店
+// 搜索框
+// 输入 应用名称
+// 魅族应用商店   com.meizu.mstore / id("mc_search_edit").setText("") / id("mc_tvlayout")
